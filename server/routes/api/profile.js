2e94cfdf77056aee4b3fdfcc7d5a909498c3b636
jeronimomora <jeronimomora@berkeley.edu>
2019-10-29 22:43:09 -0700

    Add a few profile routes

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import auth from '~/server/middleware/auth'
 import Profile from '~/server/models/Profile'
+import User from '~/server/models/User'
 import { check, validationResult } from 'express-validator'
 
 const router = express.Router()
@@ -109,7 +110,6 @@ router.post('/', [
     console.error(error.message)
     res.status(500).send('Server error')
   }
-  res.send('yeet')
 })
 
 // @route   GET api/profile
@@ -142,6 +142,89 @@ router.get('/user/:user_id', async (req, res) => {
     if (error.kind === 'ObjectId') {
       return res.status(400).json({ msg: 'Profile not found' })
     }
+    res.status(500).send('Server error')
+  }
+})
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user, and posts
+// @access  Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id })
+
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id })
+
+    res.json({ msg: 'User deleted' })
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server error')
+  }
+})
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+
+router.put('/experience', [
+  auth,
+  [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+  ]
+], async (req, res) => {
+  try {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+
+      profile.experience.unshift(newExp)
+
+      await profile.save()
+
+      res.json(profile)
+    } catch (error) {
+      console.error(error.message)
+      return res.status(500).send('Server Error')
+    }
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id })
+
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id })
+
+    res.json({ msg: 'User deleted' })
+  } catch (error) {
+    console.error(error.message)
     res.status(500).send('Server error')
   }
 })
